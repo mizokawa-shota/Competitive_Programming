4c1c8b49bd89ee2dfb1d9541afe7776bc1bf1dcc
Ikazuchi-thunder <ikazuchisdiary@gmail.com>
2017-06-13 10:17:54 +0900

    リファクタリング

--- ABC064/D.py
+++ ABC064/D.py
@@ -4,26 +4,18 @@ S = input()
 output = ''
 
 hiraki = 0
-toji = 0
 for i in S:
     if i == '(':
-        if toji > 0:
-            output = '(' * toji + output
-            toji = 0
-            hiraki += 1
-            output += i
-        else:
-            hiraki += 1
-            output += i
+        hiraki += 1
+        output += i
     else:
         if hiraki > 0:
             hiraki -= 1
             output += i
         else:
-            toji += 1
+            output = '(' + output
             output += i
 
 output += ')' * hiraki
-output = '(' * toji + output
 
 print(output)
